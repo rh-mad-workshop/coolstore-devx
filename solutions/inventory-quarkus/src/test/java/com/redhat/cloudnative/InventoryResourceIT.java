4d0f9d16db1de8aeffeea71761a122cf92861761
Daniel Oh <doh@redhat.com>
2024-04-17 23:13:27 -0400

    remove solution

--- solutions/inventory-quarkus/src/test/java/com/redhat/cloudnative/InventoryResourceIT.java
+++
@@ -1,9 +0,0 @@
-package com.redhat.cloudnative;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-public class InventoryResourceIT extends InventoryResourceTest {
-
-    // Execute the same tests but in packaged mode.
-}
